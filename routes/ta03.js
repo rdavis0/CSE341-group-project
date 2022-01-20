b545745d9beab3e14d3897431b52a837a116262e
rdavis0 <rdfiasco+github@gmail.com>
2022-01-19 20:30:00 -0700

    part of ta03

--- routes/ta03.js
+++ routes/ta03.js
@@ -2,13 +2,22 @@
 const express = require('express');
 const router = express.Router();
 
+const fs = require('fs');
+const path = require('path');
+
 router.get('/', (req, res, next) => {
-  res.render('pages/ta03', {
-    title: 'Team Activity 03',
-    path: '/ta03', // For pug, EJS
-    activeTA03: true, // For HBS
-    contentCSS: true, // For HBS
-  });
+    const p = path.join(__dirname, '..', 'data', 'items.json');
+    fs.readFile(p, (err, data) => {
+        const products = JSON.parse(data);
+
+        res.render('pages/ta03', {
+            title: 'Team Activity 03',
+            path: '/ta03', // For pug, EJS
+            products: products,
+            activeTA03: true, // For HBS
+            contentCSS: true, // For HBS
+        });
+    })
 });
 
 module.exports = router;
